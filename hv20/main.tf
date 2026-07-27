b62971b72a231ea1e98710a1b3968629b330a822
Maxism_Zhmyhov <maax@i.ua>
2026-07-27 22:07:38 +0300

    HW20+1

--- hv20/main.tf
+++ hv20/main.tf
@@ -12,3 +12,22 @@ module "my_nginx" {
 output "nginx_check_url" {
   value = module.my_nginx.nginx_url
 }
+
+terraform {
+  required_version = ">1.10"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "ft-state-984140674362"
+    region       = "eu-central-1"
+    key          = "ng/ng/terraform.tfstate"
+    use_lockfile = true
+  }
+}
+
